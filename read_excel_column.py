0da60a8fdbc0c5748dd209342499da428d880a12
Rick Li <ADreamCareer@gmail.com>
2020-04-10 16:08:34 +0800

    refactor

--- read_excel_column.py
+++ read_excel_column.py
@@ -8,21 +8,24 @@ def read_excel_column(path,sheet,column):
 def read_done(path,column):
     read_excel_column(path,'已配置',column)
 
-def read_done_out_device_name(path):
-    read_done(path,'开出设备名称')
-def read_done_out_port(path):
-    read_done(path,'开出端子号')
-def read_done_out_describe(path):
-    read_done(path,'开出端子描述')
-def read_done_out_ref(path):
-    read_done(path,'开出端子引用')
-
-def read_done_in_device_name(path):
-    read_done(path,'开入设备名称')
-def read_done_in_port(path):
-    read_done(path,'开入端子号')
-def read_done_in_describe(path):
-    read_done(path,'开入端子描述')
-def read_done_in_ref(path):
-    read_done(path,'开入端子引用')
+def read_done_out(path, columnTail):
+    read_done(path,'开出' + columnTail)
+def read_done_outDeviceName(path):
+    read_done_out(path,'设备名称')
+def read_done_outPortNum(path):
+    read_done_out(path,'端子号')
+def read_done_outDescribe(path):
+    read_done_out(path,'端子描述')
+def read_done_outRef(path):
+    read_done_out(path,'端子引用')
+def read_done_in(path, columnTail):
+    read_done(path,'开入' + columnTail)
+def read_done_inDeviceName(path):
+    read_done_in(path,'设备名称')
+def read_done_inPortNum(path):
+    read_done_in(path,'端子号')
+def read_done_inDescribe(path):
+    read_done_in(path,'端子描述')
+def read_done_inRef(path):
+    read_done_in(path,'端子引用')
 
