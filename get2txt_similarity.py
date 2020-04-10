f42704ecb56bc3fc56ab831c8501daee4de27a8c
Rick Li <ADreamCareer@gmail.com>
2020-04-10 17:01:36 +0800

    refactor

--- get2txt_similarity.py
+++ get2txt_similarity.py
@@ -3,6 +3,8 @@ from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.nlp.v20190408 import nlp_client, models
+def get2txt_similarity(t1,t2,req):
+    pass
 def get2txt_similarity(t1,t2):
     try:
         cred = credential.Credential("AKIDvc4HK8Z088tOdeS0OchbKo45rNk3uvUp", "GMErPindFrTt4vXtzrVv50mJacPyHK7o")
