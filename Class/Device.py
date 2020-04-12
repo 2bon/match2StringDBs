af0f241b4b8d83425c1e20ad13aba905fcf0f808
Rick Li <ADreamCareer@gmail.com>
2020-04-12 14:29:44 +0800

    一个设备有可能有4种不同属性的端子，GOOSE开出、GOOSE开入、SV开出和SV入, 对于一个端子只能是其中的一种属性

--- Class/Device.py
+++ Class/Device.py
@@ -5,9 +5,8 @@ from Class.Object import Object
 class Device(Object):
     chinese_name = '设备'
 
-    def __init__(self, isOut: bool, name: str, Port: Port, str):
+    def __init__(self, name: str, portList: [Port], str):
         self.str = str
 
-        self.isOut = isOut
         self.name = name
-        self.Port = Port
+        self.portList = portList
--- Class/Port.py
+++ Class/Port.py
@@ -4,7 +4,9 @@ from Class.Object import Object
 class Port(Object):
     chinese_name = '端子'
 
-    def __init__(self, num, description, reference):
+    def __init__(self,isGoose: bool,isOut: bool,  num, description, reference):
+        self.isGoose = isGoose
+        self.isOut = isOut
         self.num = num
         self.description = description
         self.reference = reference
