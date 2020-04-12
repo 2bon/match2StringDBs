be8d571d9b8d5b715523d87ac9634851c28164f3
Rick Li <ADreamCareer@gmail.com>
2020-04-12 16:51:59 +0800

    matchList

--- Class/KnowledgeBase.py
+++ Class/KnowledgeBase.py
@@ -2,19 +2,23 @@ import pandas as pd
 
 from Class import *
 from Class.Device import Device
+from Class.Match import Match
 from Class.Object import Object
 import Class
 from Class.Port import Port
 
 
 class KnowledgeBase():
-    matchList = []
+    matchList = [Match]
 
     def learn(path='../test/井门.xls'):
         sheet = pd.ExcelFile(path).parse('已配置')
         for row in sheet.iterrows():
             outPort = Port(row[1]['开出设备名称'],row[1]['开出端子号'], row[1]['开出端子描述'], row[1]['开出端子引用'])
-            print(vars(outPort))
+            inPort = Port(row[1]['开入设备名称'],row[1]['开入端子号'], 0, row[1]['开入端子引用'])
+            match = Match(outPort,inPort)
+            KnowledgeBase.matchList.append(match)
+        print((KnowledgeBase.matchList))
 
 
 def read_excel_column(path: str, sheet: str, column: str) -> list:
