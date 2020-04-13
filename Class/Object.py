51b0e34bea733f8d9e3b288c785e6a115f3ef907
Rick Li <ADreamCareer@gmail.com>
2020-04-13 20:54:28 +0800

    https://stackoverflow.com/questions/9539921/how-do-i-create-a-python-function-with-optional-arguments/9539945

--- Class/Object.py
+++ Class/Object.py
@@ -2,9 +2,9 @@
 import pandas as pd
 class Object(object):
     chinese_name = ''
-    def __init__(self, value:str):
+    def __init__(self, value:str, *args, **kwargs):
         self.value = value
-    def __init__(self, strMapList):
+    def __init__(self, strMapList, *args, **kwargs):
         for key, value in strMapList:
             if self.chinese_name in key:
                 self.value = value
