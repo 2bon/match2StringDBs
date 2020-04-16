297be88a2a591dc6f5fba019af1bf66258bce77f
Rick Li <ADreamCareer@gmail.com>
2020-04-16 14:53:28 +0800

    https://stackoverflow.com/questions/25568232/how-can-i-recursively-print-the-contents-of-a-variable-including-both-the-data

--- Class/KnowledgeBase.py
+++ Class/KnowledgeBase.py
@@ -1,5 +1,7 @@
 import glob
 import os
+import jsonpickle # pip install jsonpickle
+import json
 
 import pandas as pd
 
@@ -23,16 +25,20 @@ class KnowledgeBase():
 
     def learn_excel(path2excel='..\excel\learn/井门.xls'):
         sheet = pd.ExcelFile(path2excel).parse('已配置')
-        #print(path2excel)
+        print(path2excel)
         for row in sheet.iterrows():
             try:
                 outPort = Port(row[1]['开出设备名称'], row[1]['开出端子号'], row[1]['开出端子描述'], row[1]['开出端子引用'])
                 inPort = Port(row[1]['开入设备名称'],row[1]['开入端子号'], row[1]['开入端子描述'], row[1]['开入端子引用'])
                 match = Match(outPort, inPort)
+                print(vars(inPort))
+                print(vars(outPort))
+                serialized = jsonpickle.encode(match)
+                print(json.dumps(json.loads(serialized), indent=4) )
                 KnowledgeBase.matchList.append(match)
             except RuntimeError:
                 print(row[1])
-        print((KnowledgeBase.matchList))
+        print(dir(KnowledgeBase.matchList))
 
 
 def read_excel_column(path: str, sheet: str, column: str) -> list:
@@ -45,4 +51,4 @@ def read_excel_column(path: str, sheet: str, column: str) -> list:
     return df
 
 
-KnowledgeBase.learn_folder()
+KnowledgeBase.learn_excel()
--- preRequireInstall.ps1
+++ preRequireInstall.ps1
@@ -1,2 +1,2 @@
 #pip install -r requirements.txt
-sudo pip install tencentcloud-sdk-python scf django-excel xlrd wheel pandas
+sudo pip install jsonpickle tencentcloud-sdk-python scf django-excel xlrd wheel pandas
