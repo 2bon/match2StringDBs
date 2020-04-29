ae9306481e5f020ea056320acfdc4af7eb5c9ceb
Rick Li <ADreamCareer@gmail.com>
2020-04-29 17:22:44 +0800

    try2reindex

--- Class/KnowledgeBase.py
+++ Class/KnowledgeBase.py
@@ -1,7 +1,7 @@
 import glob
 
 import pandas as pd
-import pandas.DataFrame as df
+from pandas import DataFrame
 
 from Class.Match import Match
 from Class.Port import Port
@@ -12,7 +12,7 @@ class KnowledgeBase():
         self.matchList = matchList
         self.portListDict = {str: [Port]}
         self.portDict = {str: Port}
-        self.dfDict = {str: df}  # in,out,match
+        self.dfDict = {str: DataFrame()}  # in,out,match
 
     def learn_folder(self, path2folder='..\excel\learn/220-母线&线路-第一套合并单元&第一套合并单元'):
         for filename in glob.iglob(path2folder + '**/*.xls', recursive=True):
@@ -29,9 +29,6 @@ class KnowledgeBase():
                 inPort = Port(row[1]['开入端子描述'], row[1]['开入端子引用'])
                 match = Match(outPort, inPort)
                 self.matchList.append(match)
-                df = self.dfDict.get('开出', df)
-                key = row[1]['开出端子描述'] + row[1]['开出端子引用']
-                port = df.get(key)
         except RuntimeError:
             print(row[1])
         print(dir(self.matchList))
@@ -46,21 +43,24 @@ class KnowledgeBase():
                 portList.append(port)
                 key2 = row[1][title + '端子描述'] + title + row[1][title + '端子引用']
                 self.portDict[key2] = port
-                df = self.dfDict.get(title, df)
+                df = self.dfDict.get(title, DataFrame())
+                df2=DataFrame()
                 if sheetName == '已配置':
                     df[key2] = df.get(key2, {object: float})
                 else:  # new
-                    for done in df:
-                        done[key2] = done.get(key2, float)  # is autoFill metaData useful?
+                   if key2 not in df.index:
+                        df2.reindex([key2])
+                        print (df2.index[key2])
+
+
             self.portListDict[key] = portList
         except RuntimeError:
             print(row[1])
-        print(dir(portList))
 
 
-def load_test(self, path2excel='..\excel\learn/220-母线&线路-第一套合并单元&第一套合并单元/赤厝.xls'):
-    self.load_excel(path2excel, sheetName='所有发送', title='开出')
-    self.load_excel(path2excel, sheetName='所有接收', title='开入')
+    def load_test(self, path2excel='..\excel\learn/220-母线&线路-第一套合并单元&第一套合并单元/赤厝.xls'):
+        self.load_excel(path2excel, sheetName='所有发送', title='开出')
+        self.load_excel(path2excel, sheetName='所有接收', title='开入')
 
 
 def transform(multilevelDict):
