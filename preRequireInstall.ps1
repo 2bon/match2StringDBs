0462df62d5e8ddb0ec3e3ac3c86026575e9e4da8
Rick Li <ADreamCareer@gmail.com>
2020-05-08 11:40:23 +0800

    df.to_excel

--- preRequireInstall.ps1
+++ preRequireInstall.ps1
@@ -1,2 +1,2 @@
 #pip install -r requirements.txt
-sudo pip install jsonpickle tencentcloud-sdk-python scf django-excel xlrd wheel pandas kite
+sudo pip install jsonpickle tencentcloud-sdk-python scf django-excel xlrd wheel pandas kite openpyxl
